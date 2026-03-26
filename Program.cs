9a479b4e11850a6cfd7354f6e7a240aae40866dc
Haithm-Shamsan <Haithm-Shamsan@users.noreply.github.com>
2026-03-26 18:41:54 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using My_First_Class_Library;
+using System;
+using My_First_Class_Library;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hi");
+
+        clsMyMath MyMath1 = new clsMyMath();
+
+       
+        Console.WriteLine(MyMath1.Sum(10, 20).ToString());
+
+        Console.WriteLine(MyMath1.Sum(10, 20,30).ToString());
+
+        Console.ReadKey();
+
+
+    }
+    }
+
